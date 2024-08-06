3f00440a9d96c00689156eb5ff2f3e96a703241d
Edenilson <edenilson.dy@gmail.com>
2024-08-06 11:07:42 -0300

    atualizar aquivo App

--- primeira_aula/src/App.java
+++ primeira_aula/src/App.java
@@ -1,8 +1,11 @@
+import ContaBanco.ContaCorrente;
+import ContaBanco.ContaPoupanca;
+
 // import Animales.Cachorro;
 // import Animales.Gato;
 
-import Pessoas.Aluno;
-import Pessoas.Professor;
+// import Pessoas.Aluno;
+// import Pessoas.Professor;
 
 public class App {
 
@@ -23,14 +26,21 @@ public class App {
         // gato.miar();
 
 
-        System.out.println("Aluno");
-        Aluno aluno = new Aluno("Jose", 15, "programação");
-        aluno.apresentar();
+        // System.out.println("Aluno");
+        // Aluno aluno = new Aluno("Jose", 15, "programação");
+        // aluno.apresentar();
     
 
-        System.out.println("\nProfessor");
-        Professor professor = new Professor("Maria", 45, "programação");
-        professor.apresentar();
+        // System.out.println("\nProfessor");
+        // Professor professor = new Professor("Maria", 45, "programação");
+        // professor.apresentar();
         
+        System.out.println("Conta Corrente");
+        ContaCorrente contaCorrenteA = new ContaCorrente("12345-0", 1.50000f, 800d);
+        contaCorrenteA.exibirDetalhes();
+
+        System.out.println("\nConta Poupança");
+        ContaPoupanca contaPoupancaA = new ContaPoupanca("67890-1", 2.0000d, 100d);
+        contaPoupancaA.exibirDetalhes();
     }
 }
