8a3fb3f9292261e65a1b4532b5d818a81c1a22b1
Edenilson <edenilson.dy@gmail.com>
2024-08-06 11:36:09 -0300

    App.java atualizado

--- primeira_aula/src/App.java
+++ primeira_aula/src/App.java
@@ -1,5 +1,8 @@
-import ContaBanco.ContaCorrente;
-import ContaBanco.ContaPoupanca;
+import Veiculos.Carro;
+import Veiculos.Moto;
+
+// import ContaBanco.ContaCorrente;
+// import ContaBanco.ContaPoupanca;
 
 // import Animales.Cachorro;
 // import Animales.Gato;
@@ -35,12 +38,20 @@ public class App {
         // Professor professor = new Professor("Maria", 45, "programação");
         // professor.apresentar();
         
-        System.out.println("Conta Corrente");
-        ContaCorrente contaCorrenteA = new ContaCorrente("12345-0", 1.50000f, 800d);
-        contaCorrenteA.exibirDetalhes();
+        // System.out.println("Conta Corrente");
+        // ContaCorrente contaCorrenteA = new ContaCorrente("12345-0", 1.50000f, 800d);
+        // contaCorrenteA.exibirDetalhes();
 
-        System.out.println("\nConta Poupança");
-        ContaPoupanca contaPoupancaA = new ContaPoupanca("67890-1", 2.0000d, 100d);
-        contaPoupancaA.exibirDetalhes();
+        // System.out.println("\nConta Poupança");
+        // ContaPoupanca contaPoupancaA = new ContaPoupanca("67890-1", 2.0000d, 100d);
+        // contaPoupancaA.exibirDetalhes();
+
+        System.out.println("Carro");
+        Carro meuCarro = new Carro("Toyota", 2024, 4);
+        meuCarro.detalhes();
+
+        System.out.println("\nMoto");
+        Moto minhaMoto = new Moto("Honda", 2018, 2);
+        minhaMoto.detalhes();
     }
 }
